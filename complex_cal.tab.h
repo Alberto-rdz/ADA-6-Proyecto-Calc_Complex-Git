fd542bf47b9f971f4078b4e0f6e0938c3e8e991c
MichaellSolis28 <michaellsolis0@gmail.com>
2022-10-22 16:44:04 -0500

    Añadí "token kinds"

--- complex_cal.tab.h
+++ complex_cal.tab.h
@@ -1,10 +1,25 @@
 #ifndef YY_YY_COMPLEX_CAL_TAB_H_INCLUDED
+# define YY_YY_COMPLEX_CAL_TAB_H_INCLUDED
 /* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
 #endif
 #if YYDEBUG
 extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    CNUMBER = 258                  /* CNUMBER  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
