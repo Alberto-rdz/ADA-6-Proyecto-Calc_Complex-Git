56ca6f3bcb3c8933d62bdd574e08e7dd01704340
MichaellSolis28 <michaellsolis0@gmail.com>
2022-10-22 16:41:32 -0500

    Se crea el archivo complex_cal.tab.h

---
+++ complex_cal.tab.h
@@ -0,0 +1,24 @@
+#ifndef YY_YY_COMPLEX_CAL_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_COMPLEX_CAL_TAB_H_INCLUDED  */
